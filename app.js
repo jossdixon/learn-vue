076fce64ee9600cdcd74744e78471373bac9b6cd
Joss Dixon <josscdixon@gmail.com>
2022-01-14 15:17:01 +0000

    more work done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
     return {
       firstName: 'Peter',
       lastName: 'Peterson',
-      email: 'boy@wonder.com',
+      email: 'em@il.com',
       gender: 'male',
       picture: 'https://randomuser.me/api/portraits/men/26.jpg',
     }
