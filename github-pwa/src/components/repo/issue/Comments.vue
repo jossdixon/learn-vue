df13fc7d083bc26c8bffdd9803acbbad8e52f699
Joss Dixon <josscdixon@gmail.com>
2022-02-16 14:16:13 +0000

    update

---
+++ github-pwa/src/components/repo/issue/Comments.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-if="comments.length > 0">
+      <h4>Comments</h4>
+      <div v-for="c of comments" :key="c.id">
+        {{c.user && c.user.login}} - {{c.body}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { octokitMixin } from '../../../mixins/octokitMixin';
+
+export default defineComponent({
+  setup() {
+
+  },
+})
+</script>
