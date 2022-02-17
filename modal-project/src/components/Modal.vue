a1e9a4d8f854b3d1b3ec86819be443943fc5bd2f
Joss Dixon <josscdixon@gmail.com>
2022-02-17 19:43:13 +0000

    adding modal

---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>Modal Content</p>
+    </div>
+  </div>
+</template>
+
+<style>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: whitesmoke;
+    border-radius: 8px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: darkslategrey;
+    width: 100%;
+    height: 100%;
+  }
+</style>
