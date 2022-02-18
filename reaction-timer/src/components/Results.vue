eb0eea16db899cb761ab12e0f7757080d2577005
Joss Dixon <josscdixon@gmail.com>
2022-02-18 10:52:26 +0000

    reaction timer game completed

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,10 +1,25 @@
 <template>
   <h2>Your score is {{score}} ms.</h2>
+  <h3>You have the speed of {{rank}}</h3>
 </template>
 
 <script>
 export default {
-  props: ['score']
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "a demon's whisper."
+    } else if (this.score < 400) {
+      this.rank = 'an able seaman.'
+    } else {
+      this.rank = 'the most sluggish buffoon.'
+    }
+  }
 }
 </script>
 
